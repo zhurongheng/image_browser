8b01a3ced20b8142df804c70fa2cd0892efe4678
zhurongheng <1406483087@qq.com>
2017-11-13 00:42:56 +0800

    修复百度地图显示问题

--- js/directives.js
+++ js/directives.js
@@ -2,47 +2,42 @@ angular.module('myApp')
 /**
  * 动态设置图片选择滚动条高度
  */
-    .directive('imageScroll', function($compile) {
+    .directive('imageScroll', function ($compile) {
+        return {
+            restrict: 'AE',
+            link: function (scope, element, attr, ctrl) {
+                element[0].style.height = (window.innerHeight - 508) + 'px';
+            }
+        };
+    }).directive('minMap', function ($compile) {
     return {
         restrict: 'AE',
-        link: function(scope, element, attr, ctrl) {
-           element[0].style.height=(window.innerHeight-508)+'px';
-        }
-    };
-}).directive('minMap', function($compile) {
-    return {
-        restrict: 'AE',
-        require:'ngModel',
-        scope:{
-            ngModel:'=',
-            closeMap:'&'
+        require: 'ngModel',
+        scope: {
+            ngModel: '=',
+            closeMap: '&'
         },
-        link: function(scope, element, attr, ctrl) {
-            var div=document.createElement('div');
-            div.style.width=window.innerWidth;
-            div.style.height=window.innerHeight-44+'px';
-            console.log('w:'+window.innerWidth);
-            console.log('h:'+window.innerHeight)
-            element[0].appendChild(div);
+        link: function (scope, element, attr, ctrl) {
             var options = {
-                width:140,
-                height:80,
+                width: 140,
+                height: 80,
+                elementSuper: element[0],
                 title: '<b>位置信息</b>',
                 result: function (info) {
                     console.log(info);
                     scope.$apply(function () {
-                        scope.ngModel=info;
+                        scope.ngModel = info;
                     })
                 },
-                closeMap:function(){
+                closeMap: function () {
                     scope.$apply(function () {
-                        if(typeof(scope.closeMap)=='function'){
+                        if (typeof(scope.closeMap) == 'function') {
                             scope.closeMap();
                         }
                     });
                 }
             }
-            $map.minMap(div).build(options);
+            $map.minMap().build(options);
         }
     };
 });
